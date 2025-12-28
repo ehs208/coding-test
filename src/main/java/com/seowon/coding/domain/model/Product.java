15fa6e425a10a113963452e0394896ca5b936e60
EunHyunsu <ehs208@naver.com>
2025-12-28 17:38:36 +0900

    refactor: 대량 가격 변경 로직 도메인객체 안으로 리팩토링

--- src/main/java/com/seowon/coding/domain/model/Product.java
+++ src/main/java/com/seowon/coding/domain/model/Product.java
@@ -12,6 +12,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Entity
 @Data
@@ -55,5 +56,15 @@ public class Product {
         stockQuantity += quantity;
     }
 
+    public void updatePrice(double percentage, boolean includeTax) {
+        double base = this.getPrice() == null ? 0.0 : this.getPrice().doubleValue();
+                    double changed = base + (base * (percentage / 100.0)); // 부동소수점 오류 가능
+                    if (includeTax) {
+                        changed = changed * 1.1; // 하드코딩 VAT 10%, 지역/카테고리별 규칙 미반영
+                    }
+                    // 임의 반올림: 일관되지 않은 스케일/반올림 모드
+                    BigDecimal newPrice = BigDecimal.valueOf(changed).setScale(2, RoundingMode.HALF_UP);
+                    this.setPrice(newPrice);
+    }
 
 }
